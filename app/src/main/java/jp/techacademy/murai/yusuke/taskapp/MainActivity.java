8288b6baf8a1c2fbaacd3406c97bfdeb57e097ac
ym.devtech <ym.devtech@gmail.com>
2017-01-09 18:00:20 +0900

    Initial commit 4.5まで実装済み

---
+++ app/src/main/java/jp/techacademy/murai/yusuke/taskapp/MainActivity.java
@@ -0,0 +1,78 @@
+package jp.techacademy.murai.yusuke.taskapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // ListViewの設定
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView) findViewById(R.id.listView1);
+
+        // ListViewをタップしたときの処理
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 入力・編集する画面に遷移させる
+            }
+        });
+
+        // ListViewを長押ししたときの処理
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // タスクを削除する
+
+                return true;
+            }
+        });
+
+
+
+
+        reloadListView();
+    }
+
+    private void reloadListView() {
+        // 後でTaskクラスに変更する
+        ArrayList<String> taskArrayList = new ArrayList<>();
+
+        taskArrayList.add("aaa");
+        taskArrayList.add("bbb");
+        taskArrayList.add("ccc");
+
+        mTaskAdapter.setTaskArrayList(taskArrayList);
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
+
+    }
+}
---
+++ app/src/main/java/jp/techacademy/murai/yusuke/taskapp/TaskAdapter.java
@@ -0,0 +1,57 @@
+package jp.techacademy.murai.yusuke.taskapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by my99workmac on 2017/01/09.
+ */
+
+public class TaskAdapter extends BaseAdapter{
+    private LayoutInflater mLayoutInflater;
+    private ArrayList<String> mTaskArrayList;
+
+    public TaskAdapter(Context context) {
+        mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void setTaskArrayList(ArrayList<String> taskArrayList) {
+        mTaskArrayList = taskArrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return mTaskArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mTaskArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null);
+        }
+
+        TextView textView1 = (TextView) convertView.findViewById(android.R.id.text1);
+        TextView textView2 = (TextView) convertView.findViewById(android.R.id.text2);
+
+        // 後でTaskクラスから情報を取得するように変更する
+        textView1.setText(mTaskArrayList.get(position));
+
+        return convertView;
+    }
+}

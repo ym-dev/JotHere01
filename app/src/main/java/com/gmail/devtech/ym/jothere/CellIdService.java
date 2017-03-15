8d768953a37567b4bc760ca2244291eaad3ac5d9
ym-dev <ym.devtech@gmail.com>
2017-03-16 02:01:21 +0900

    CellIDが変更されたときにNotificationバーで通知するよう実装完了

--- app/src/main/java/com/gmail/devtech/ym/jothere/CellIdService.java
+++ app/src/main/java/com/gmail/devtech/ym/jothere/CellIdService.java
@@ -25,8 +25,6 @@ import java.util.Locale;
 public class CellIdService extends Service {
     public static final String TAG = "JotHereLog";
     public Integer cellId;
-
-
     public TelephonyManager TM;
 
     public CellIdService() {
@@ -36,6 +34,15 @@ public class CellIdService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "CellIdCervice onStartCommandが呼ばれました");
+
+/*        //sendNotifi実験用コード。サービス起動５秒後に発動
+        try {
+            Thread.sleep(5000);
+            Integer testId = 111111;
+            sendNotification(testId);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
 
         //電話情報の受信開始
         TelephonyManager telManager=(TelephonyManager)
@@ -73,7 +80,7 @@ public class CellIdService extends Service {
             }
             Log.d(TAG, str);
 
-                sendNotification(cellId);
+            sendNotification(cellId);
         }
     };
 
@@ -100,6 +107,7 @@ public class CellIdService extends Service {
         Date now = new Date(System.currentTimeMillis());
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.JAPANESE);
         String nowText = formatter.format(now);
+        Log.d(TAG, "nowTime="+nowText);
         Notification notification;
 
         string = cellid.toString();
@@ -108,9 +116,9 @@ public class CellIdService extends Service {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 
         builder.setAutoCancel(true);
-        builder.setContentTitle("SAMP!");
-        builder.setContentText(string);
-        builder.setSubText(nowText);
+        builder.setContentTitle("Cell ID Changed!");
+        builder.setContentText("CID="+string);
+//        builder.setSubText(nowText);
         builder.setSmallIcon(android.R.drawable.ic_dialog_info);
 //        builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setContentIntent(pendingIntent);
@@ -123,60 +131,7 @@ public class CellIdService extends Service {
 
         manager.notify(0, builder.build());
     }
-/*
 
-
-    private void sendNotification() {
-        Log.d(TAG, "sendNotification()");
-
-
-        String string = "";
-        //Intent intent = new Intent();       //引数なしのIntentを作成。通知をタップしても何もしない。
-
-        // 通知の設定を行う
-        NotificationCompat.Builder builder = new NotificationCompat.Builder();
-        builder.setSmallIcon(R.drawable.small_icon);
-        builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.large_icon));
-        builder.setWhen(System.currentTimeMillis());
-        builder.setDefaults(Notification.DEFAULT_ALL);
-        builder.setAutoCancel(true);
-
-        // 通知をタップしたらアプリを起動するようにする
-        Intent startAppIntent = new Intent(this, MainActivity.class);
-        startAppIntent.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, startAppIntent, 0);
-        builder.setContentIntent(pendingIntent);
-
-
-        Intent intent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-        Date now = new Date(System.currentTimeMillis());
-        DateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 HH時mm分");
-        String nowText = formatter.format(now);
-        Notification notification;
-
-        string = ;
-
-        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-
-        builder.setAutoCancel(true);
-        builder.setContentTitle("SAMP!");
-        builder.setContentText(string);
-        builder.setSubText(nowText);
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentIntent(pendingIntent);
-        //builder.setDefaults(Notification.DEFAULT_VIBRATE);
-        builder.setAutoCancel(false);
-        notification = builder.build();
-
-        notification.flags |= Notification.FLAG_ONGOING_EVENT;
-
-
-        manager.notify(0, builder.build());
-
-    }
-*/
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
